70886fbf82c8b385380d0ba06d3ab0e8ca14c754
Беглецов Сергей Николаевич <snower87@yandex.ru>
2026-04-09 19:30:54 +0300

    1.9 Статические файлы

---
+++ 1_FastAPI_base/1_9_static_files/1_9_static_files.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Пример 1 - при обращении по пути "/static" приложение будет посылать в ответ файлы из каталога "public".
+app.mount("/static", StaticFiles(directory="example"))
+
+# Пример 2 - Получение файла index.html (при выставленном параметре html=True)
+# Если значение html=True, то сервер автоматически отправляет файл index.html (при его наличии)
+# Если значение html=False (по умолчанию), то
+# Теперь мы можем обратиться по пути http://127.0.0.1:8000/static/, и сервер также пришлет нам страницу index.html:
+app.mount("/static", StaticFiles(directory="example", html=True))
+
+# Пример 3 - установить главную страницу для всего веб-приложения
+app.mount("/", StaticFiles(directory=".", html=True))
